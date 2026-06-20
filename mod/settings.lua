794343b055fa8bac42ebf4ed36b3ac8785f179a1
Alex Wittman <wittmanalex@gmail.com>
2026-06-20 08:22:20 -0600

    :sparkles: Add shadow setting and en localization

---
+++ mod/settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "wire-art-enable-wire-shadows",
+        setting_type = "startup",
+        default_value = false,
+        order = "a"
+    }
+})
--- mod/wire_data.lua
+++ mod/wire_data.lua
@@ -4,13 +4,14 @@ local blue = { r = 0, g = 0, b = 1, a = 1 }
 
 local function set_world_wire(name, tint)
     local sprite = data.raw["utility-sprites"]["default"][name]
-    sprite.filename = "__wire_art__/graphics/hr-generic-wire_white_0.2.png"
+    sprite.filename = "__wire_art__/graphics/hr-generic-wire.png"
     sprite.tint = tint
     sprite.blend_mode = "additive-soft"
 end
 
--- TODO: Add global startup setting for this in case you want shadows
-data.raw["utility-sprites"]["default"].wire_shadow.filename = "__core__/graphics/empty.png"
+if not settings.startup["wire-art-enable-wire-shadows"].value then
+    data.raw["utility-sprites"]["default"].wire_shadow.filename = "__core__/graphics/empty.png"
+end
 
 set_world_wire("red_wire", red)
 set_world_wire("green_wire", green)
